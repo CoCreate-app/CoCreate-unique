fc75bda105c0427dd3890ea9a76d55e596824d3a
Frank <frank@cocreate.app>
2022-03-03 03:55:14 +0000

    feat: manage server and client scripts

--- src/index.js
+++ src/index.js
@@ -1,65 +1,14 @@
-import crud from '@cocreate/crud-client';
-import uuid from '@cocreate/uuid';
-
-const CoCreateUnique = {
-
-	selector: `input[unique], textarea[unique], contenteditable[unique]`,
-	requestAttr: "data-unique_request_id",
-	
-	init: function(container) {
-		
-		let mainContainer = container || document;
-		
-		if (!mainContainer.querySelectorAll) {
-			return;
-		}
-		
-		let items = mainContainer.querySelectorAll(this.selector)
-		const self = this;
-		
-		items.forEach((item) => {
-			const request_id = uuid.generate();
-			item.setAttribute(self.requestAttr, request_id);
-			self.setInputEvent(item)
-		})
-	},
-	
-	initListener: function() {
-		const self = this;
-		crud.listen('checkedUnique', function(data) {
-			self.checkedUnique(data)
-		})	
-	},
-	
-	checkedUnique: function(data) {
-		const request_id = data['request_id'];
-		
-		if (!request_id) return
-		
-		const element = document.querySelector(`[${this.requestAttr}='${request_id}']`)
-		
-		if (data['unique']) {
-			element.setAttribute('unique', true);
-		} else {
-			element.setAttribute('unique', false);
-		}
-	},
-
-	setInputEvent: function(input) {
-		const self = this;
-		input.addEventListener('input', function(e) {
-			let request_data = {};
-			let value = input.getValue(input);
-			request_data['organization_id'] = window.config.organization_Id;
-			request_data['apiKey'] = window.config.apiKey;
-			request_data['collection'] = input.getAttribute('collection');
-			request_data['name'] = input.getAttribute('name');
-			request_data['value'] = value || e.target.value;
-			request_data['request_id'] = input.getAttribute(self.requestAttr);
-			crud.send('checkUnique', request_data);
-		});
-	},
-};
-
-CoCreateUnique.init();
-CoCreateUnique.initListener();
+(function (root, factory) {
+    if (typeof define === 'function' && define.amd) {
+        define(["./client"], function(CoCreateUnique) {
+        	return factory(CoCreateUnique)
+        });
+    } else if (typeof module === 'object' && module.exports) {
+      const CoCreateUnique = require("./server.js")
+      module.exports = factory(CoCreateUnique);
+    } else {
+        root.returnExports = factory(root["./client.js"]);
+  }
+}(typeof self !== 'undefined' ? self : this, function (CoCreateUnique) {
+  return CoCreateUnique;
+}));
---
+++ src/server.js
@@ -0,0 +1,45 @@
+class CoCreateUnique {
+	constructor(wsManager, dbClient) {
+		this.wsManager = wsManager
+		this.dbClient = dbClient
+		this.init();
+	}
+	
+	init() {
+		if (this.wsManager) {
+			this.wsManager.on('checkUnique', 
+				(socket, data, roomInfo) => this.checkUnique(socket, data, roomInfo));
+		}
+	}
+
+
+	async checkUnique(socket, req_data) {
+		const self = this
+		const db = this.dbClient.db(req_data['organization_id']);
+		const collection = db.collection(req_data["collection"]);
+		const query = {
+			[req_data['name']]: req_data['value']
+		};
+		
+		try {
+			collection.find(query).toArray(function(error, result) {
+				if (!error && result) {
+					let response = {
+						request_id: req_data['request_id'],
+						name: req_data['name'],
+						unique: true
+					};
+					if (result.length) {
+						response.unique = false;
+					}
+					self.wsManager.send(socket, 'checkedUnique', response, req_data['organization_id']);
+				}
+			});
+		} catch (error) {
+			console.log(error);
+		}
+	}
+	
+}
+
+module.exports = CoCreateUnique;
