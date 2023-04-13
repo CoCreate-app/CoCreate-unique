51ff6c57de1b217767763e7d06ca13fe4c216cac
Frank Pagan <frank@cocrete.app>
2023-04-13 02:32:50 -0600

    feat: supports checking indexeddb and server db

--- src/index.js
+++ src/index.js
@@ -1,14 +1,63 @@
-(function (root, factory) {
-    if (typeof define === 'function' && define.amd) {
-        define(["./client"], function(CoCreateUnique) {
-        	return factory(CoCreateUnique)
-        });
-    } else if (typeof module === 'object' && module.exports) {
-      const CoCreateUnique = require("./server.js")
-      module.exports = factory(CoCreateUnique);
-    } else {
-        root.returnExports = factory(root["./client.js"]);
-  }
-}(typeof self !== 'undefined' ? self : this, function (CoCreateUnique) {
-  return CoCreateUnique;
-}));
+import crud from '@cocreate/crud-client';
+
+const CoCreateUnique = {
+
+	selector: `input[unique], textarea[unique], contenteditable[unique]`,
+	
+	init: function(container) {
+		
+		let mainContainer = container || document;
+		
+		if (!mainContainer.querySelectorAll) {
+			return;
+		}
+		
+		let items = mainContainer.querySelectorAll(this.selector)
+		const self = this;
+		
+		items.forEach((item) => {
+			self.setInputEvent(item)
+		})
+	},
+		
+	checkedUnique: function(data) {
+		const element = data.input
+		if (data['unique'])
+			element.setAttribute('unique', true);
+		else
+			element.setAttribute('unique', false);
+	},
+
+	setInputEvent: function(input) {
+		const self = this;
+		input.addEventListener('input', function(e) {
+			let name = input.getAttribute('name');
+			let value = input.getValue();
+			let request = {
+				collection: input.getAttribute('collection'),
+				filter: {
+					query: [{
+						name, 
+						value,
+						operator: '$eq'
+					}]
+				}
+			};
+
+			crud.readDocument(request).then((data) => {
+				let response = {
+					input,
+					name,
+					unique: true
+				};
+				if (data.document && data.document.length) {
+					response.unique = false;
+				}
+				self.checkedUnique(response)
+			})
+
+		});
+	},
+};
+
+CoCreateUnique.init();
