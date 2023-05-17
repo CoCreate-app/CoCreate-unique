5469faae84157d45b1fa583e1c20336cbeb8b500
frankpagan <frank@cocreate.app>
2023-05-17 13:43:25 -0600

    feat: Add unique input check and server implementation
    This commit adds the ability to check if an input's value is unique when they have the "unique" attribute. It also includes a new server implementation to handle the unique check. The CoCreateUnique module was added to index.js, and the server.js and client.js files were also modified.

---
+++ src/client.js
@@ -0,0 +1,60 @@
+import crud from '@cocreate/crud-client';
+
+function init() {
+    let elements = document.querySelectorAll('[unique]')
+    initElements(elements)
+}
+
+function initElements(elements) {
+    for (let element of elements)
+        initElement(element)
+}
+
+function initElement(element) {
+    setInputEvent(element)
+}
+
+function setInputEvent(input) {
+    let delayTimer
+
+    input.addEventListener('input', function (e) {
+        clearTimeout(delayTimer);
+        delayTimer = setTimeout(function () {
+            isUnique(input)
+        }, 1000);
+    });
+}
+
+async function isUnique(element) {
+    let name = element.getAttribute('name');
+    let value = element.getValue();
+    let request = {
+        db: 'indexeddb',
+        collection: element.getAttribute('collection'),
+        filter: {
+            query: [{
+                name,
+                value,
+                operator: '$eq'
+            }]
+        }
+    };
+
+    let data = await crud.readDocument(request)
+    let response = {
+        element,
+        name,
+        unique: true
+    };
+    if (data.document && data.document.length) {
+        response.unique = false;
+    }
+    if (response.unique) {
+        delete request.db
+        response = await crud.socket.send('isUnique', request)
+    }
+
+    checkedUnique(response)
+}
+
+init();
---
+++ src/server.js
@@ -0,0 +1,36 @@
+class CoCreateUnique {
+    constructor(crud) {
+        this.wsManager = crud.wsManager
+        this.crud = crud
+        this.init();
+    }
+
+    init() {
+        if (this.wsManager) {
+            this.wsManager.on('isUnique',
+                (socket, data) => this.isUnique(socket, data));
+        }
+    }
+
+
+    async isUnique(socket, data) {
+        const self = this
+        try {
+            this.crud.readDocument(data).then((data) => {
+                let response = {
+                    name: data['name'],
+                    unique: true
+                };
+                if (data.document.length) {
+                    response.unique = false;
+                }
+                self.wsManager.send(socket, 'isUnique', response);
+            })
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+}
+
+module.exports = CoCreateUnique;
